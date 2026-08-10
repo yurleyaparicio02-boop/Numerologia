f47dfedce87704bbe21ea5e06c76742a4f20e096
yurleyaparicio02-boop <yurleyaparicio02@gmail.com>
2026-08-10 11:09:03 -0500

    Delete date_base directory

--- date_base/cnxmongo.js
+++
@@ -1,7 +0,0 @@
-import mongoose from"mongoose";
-const cnxMongoDB = ()=>{
-    mongoose.connect("mongodb://localhost:27017/astrologia")
-    .then(()=> console.log("conexion exitosa a la base de datos"));
-};
-
-export default cnxMongoDB;
